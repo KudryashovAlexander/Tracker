3bcd3ea9eaea519a69c59c630c7cc2cf84147351
KudryashovAlexander <sasha080193@gmail.com>
2023-07-30 00:55:30 +0300

    add Fonts

---
+++ Tracker/Helper/UIFont_ext.swift
@@ -0,0 +1,18 @@
+//
+//  UIFont_ext.swift
+//  Tracker
+//
+//  Created by Александр Кудряшов on 30.07.2023.
+//
+
+import UIKit
+
+extension UIFont {
+    static var yPBold34: UIFont { UIFont.boldSystemFont(ofSize: 34) }
+    static var yPBold32: UIFont { UIFont.boldSystemFont(ofSize: 32) }
+    static var yPBold19: UIFont { UIFont.boldSystemFont(ofSize: 19) }
+    static var yPRegular17: UIFont { UIFont.systemFont(ofSize: 17) }
+    static var yPMedium16: UIFont { UIFont.systemFont(ofSize: 16, weight: .medium)}
+    static var yPMedium12: UIFont { UIFont.systemFont(ofSize: 12, weight: .medium)}
+    static var yPMedium10: UIFont { UIFont.systemFont(ofSize: 10, weight: .medium)}
+}
