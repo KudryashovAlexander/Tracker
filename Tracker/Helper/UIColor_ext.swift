5e8545af4ac6fbc427abc5f5050ab03a73e42523
KudryashovAlexander <sasha080193@gmail.com>
2023-07-30 00:31:40 +0300

    Extension UIColor

---
+++ Tracker/Helper/UIColor_ext.swift
@@ -0,0 +1,36 @@
+//
+//  UIColor_ext.swift
+//  Tracker
+//
+//  Created by Александр Кудряшов on 29.07.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "ypBlack") ?? UIColor.black }
+    static var ypWhite: UIColor { UIColor(named: "ypWhite") ?? UIColor.white }
+    static var ypRed: UIColor { UIColor(named: "ypRed") ?? UIColor.red }
+    static var ypBlue: UIColor { UIColor(named: "ypBlue") ?? UIColor.blue }
+    static var ypGray: UIColor { UIColor(named: "ypGray") ?? UIColor.gray }
+    static var ypLightGray: UIColor { UIColor(named: "ypLightGray") ?? UIColor.lightGray }
+    static var ypBackground: UIColor { UIColor(named: "ypBackground") ?? UIColor.gray }
+    static var ypCS1: UIColor { UIColor(named: "ypCS1") ?? UIColor.red }
+    static var ypCS2: UIColor { UIColor(named: "ypCS2") ?? UIColor.orange }
+    static var ypCS3: UIColor { UIColor(named: "ypCS3") ?? UIColor.blue }
+    static var ypCS4: UIColor { UIColor(named: "ypCS4") ?? UIColor.purple }
+    static var ypCS5: UIColor { UIColor(named: "ypCS5") ?? UIColor.green }
+    static var ypCS6: UIColor { UIColor(named: "ypCS6") ?? UIColor.systemPink }
+    static var ypCS7: UIColor { UIColor(named: "ypCS7") ?? UIColor.blue }
+    static var ypCS8: UIColor { UIColor(named: "ypCS8") ?? UIColor.green }
+    static var ypCS9: UIColor { UIColor(named: "ypCS9") ?? UIColor.systemBlue }
+    static var ypCS10: UIColor { UIColor(named: "ypCS10") ?? UIColor.red }
+    static var ypCS11: UIColor { UIColor(named: "ypCS11") ?? UIColor.red }
+    static var ypCS12: UIColor { UIColor(named: "ypCS12") ?? UIColor.systemPink }
+    static var ypCS13: UIColor { UIColor(named: "ypCS13") ?? UIColor.yellow }
+    static var ypCS14: UIColor { UIColor(named: "ypCS14") ?? UIColor.blue }
+    static var ypCS15: UIColor { UIColor(named: "ypCS15") ?? UIColor.purple }
+    static var ypCS16: UIColor { UIColor(named: "ypCS16") ?? UIColor.purple }
+    static var ypCS17: UIColor { UIColor(named: "ypCS17") ?? UIColor.purple }
+    static var ypCS18: UIColor { UIColor(named: "ypCS18") ?? UIColor.green }
+}
