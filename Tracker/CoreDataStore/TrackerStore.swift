1725c9aea986835505b4d61e7597644b3f14c7a3
KudryashovAlexander <sasha080193@gmail.com>
2023-09-02 14:09:36 +0300

    add TrackerStore

--- Tracker/CoreDataStore/TrackerStore.swift
+++ Tracker/CoreDataStore/TrackerStore.swift
@@ -8,8 +8,46 @@
 import UIKit
 import CoreData
 
+enum TrackerStoreError: Error {
+    case decodingErrorInvalidName
+    case decodingErrorInvalidColorHex
+    case decodingErrorInvalidEmojie
+    case decodingErrorInvalidSchedule
+    case decodingErrorInvalidId
+}
+
 class TrackerStore: NSObject {
     
+    func updateTrackerCoreData(_ trackerCoreData: TrackerCoreData, with tracker: Tracker) {
+        trackerCoreData.name = tracker.name
+        trackerCoreData.colorHex = tracker.color.hexStringFromColor()
+        trackerCoreData.emojie = tracker.emojie
+        trackerCoreData.scheduleString = tracker.schedule.daysOnString()
+        trackerCoreData.id = tracker.id
+    }
     
-
+    
+    func updateTracker(_ trackerCoreData: TrackerCoreData) throws -> Tracker {
+        guard let name = trackerCoreData.name else {
+            throw TrackerStoreError.decodingErrorInvalidName
+        }
+        guard let colorHex = trackerCoreData.colorHex else {
+            throw TrackerStoreError.decodingErrorInvalidColorHex
+        }
+        guard let emojie = trackerCoreData.emojie else {
+            throw TrackerStoreError.decodingErrorInvalidEmojie
+        }
+        guard let scheduleString = trackerCoreData.scheduleString else {
+            throw TrackerStoreError.decodingErrorInvalidSchedule
+        }
+        guard let id = trackerCoreData.id else {
+            throw TrackerStoreError.decodingErrorInvalidId
+        }
+        
+        return Tracker(name: name,
+                       color: colorHex.hexStringToUIColor(),
+                       emojie: emojie,
+                       schedule: Schedule(numberDaysString: scheduleString),
+                       id: id)
+    }
 }
